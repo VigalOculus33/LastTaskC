ffb0e686d91fc0eaf1ba59c72734336c3c716cfc
VigalOculus33 <vladoffski_sv@mail.ru>
2023-08-20 22:03:58 +0500

    сделали рабочий код

---
+++ Новая папка/mmm/Program.cs
@@ -0,0 +1,35 @@
+
+
+string[] FilterArrayByLength(string[] inputArray, int maxLength)
+    {
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= maxLength)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item != null && item.Length <= maxLength)
+            {
+               newArray[index] = item;
+               index++;
+            }
+        }
+        return newArray;
+    }
+
+Console.WriteLine("Введите элементы массива, разделенные пробелом:");
+string[] inputArray = Console.ReadLine().Split(' ');
+string[] newArray = FilterArrayByLength(inputArray, 3);
+
+Console.WriteLine("Новый массив:");
+foreach (string item in newArray)
+{
+    Console.WriteLine(item);
+}
